86e3975521631b70bc51ecbf9edfd3798cdcbd85
Antoine Gagne <antoinegturcotte@gmail.com>
2017-09-28 21:46:46 -0700

    Add keywords handling for index

--- index_helper.py
+++ index_helper.py
@@ -1,5 +1,6 @@
 """Library to help manage the index file stored as a sqlite db."""
 
+import itertools
 import sqlite3
 
 
@@ -17,7 +18,9 @@ class IndexHelper():
     def initialize_db(self):
         """Initialize the db's tables."""
         self._execute("DROP TABLE IF EXISTS notes")
+        self._execute("DROP TABLE IF EXISTS keywords")
         self._execute("CREATE TABLE notes (name text, note_text text)")
+        self._execute("CREATE TABLE keywords (name text, keyword text, FOREIGN KEY(name) REFERENCES notes(name))")
         # TODO(AG) Add one-to-many for note keywords
         # TODO(AG) Add metadata table for settings and other stuff
 
@@ -25,16 +28,41 @@ class IndexHelper():
         """Create a note with given text."""
         self._execute("INSERT INTO notes VALUES (?,?)", (name, text,))
 
-    def get_note_text(self, note_name):
+    def update_note_text(self, name, text):
+        """Update the text for a note.
+
+        Args:
+          name (str): The name of the note to update
+          text (str): Text to assign to note
+        Return:
+          Int: Number of notes updated (likely 0 or 1)
+        """
+        cursor = self._execute("""UPDATE notes set note_text = ?
+                                  WHERE name=?""", (text, name,))
+        cursor = self._execute("SELECT changes()")
+        return cursor.fetchone()[0]
+
+    def get_note_text(self, name):
         """Return the text for a specific note."""
         cursor = self._execute("""SELECT note_text FROM notes
-                                  WHERE name=?""", (note_name,))
+                                  WHERE name=?""", (name,))
         return cursor.fetchone()[0]
 
     def get_notes_list(self):
         """Get the list of note names."""
-        cursor = self._exectute("SELECT name FROM notes")
+        cursor = self._execute("SELECT name FROM notes")
         return cursor.fetchall()
+
+    def add_note_keywords(self, name, keywords):
+        """Add list of keywords to a note."""
+        combinations = itertools.product([name], keywords)
+        for c in combinations:
+            self._execute("INSERT INTO keywords (name, keyword) VALUES (?,?)", (c[0],c[1],))
+
+    def get_note_keywords(self, name):
+        """Return the list of keywords for a note."""
+        cursor = self._execute("SELECT keyword FROM keywords WHERE name = ?", (name,))
+        return [x[0] for x in cursor.fetchall()]
 
     # Private functions
     def _connect_db(self, db_path):
--- index_helper_test.py
+++ index_helper_test.py
@@ -21,12 +21,56 @@ class TestNewDb(unittest.TestCase):
         # Check list of columns or something
         pass
 
+    def test_get_note_text(self):
+        note_name = "notename"
+        note_text = "notetext"
+        self.db.create_note(note_name, note_text)
+
+        text = self.db.get_note_text(note_name)
+        self.assertEqual(note_text, text)
+
+    def test_get_note_list(self):
+        note_name1 = "notename1"
+        note_name2 = "notename2"
+        note_name3 = "notename3"
+        note_text = "notetext"
+        self.db.create_note(note_name1, note_text)
+        self.db.create_note(note_name2, note_text)
+        self.db.create_note(note_name3, note_text)
+
+        notes_list = self.db.get_notes_list()
+        self.assertEqual(len(notes_list), 3)
+
     def test_create_note(self):
         note_name = "notename"
         note_text = "notetext"
         self.db.create_note(note_name, note_text)
         db_text = self.db.get_note_text(note_name)
         self.assertEqual(db_text, note_text)
+
+    def test_update_new_note(self):
+        note_name = "notename"
+        note_text = "notetext"
+        nb_changes = self.db.update_note_text(note_name, note_text)
+        self.assertEqual(nb_changes, 0)
+
+    def test_update_note(self):
+        note_name = "notename"
+        note_text_initial = "notetext"
+        note_text_final = "NoTeTeXt"
+        self.db.create_note(note_name, note_text_initial)
+        nb_changes = self.db.update_note_text(note_name, note_text_final)
+        db_text = self.db.get_note_text(note_name)
+        self.assertEqual(db_text, note_text_final)
+        self.assertEqual(nb_changes, 1)
+
+    def test_add_and_check_keywords(self):
+        note_name = "notename"
+        note_text = "notetext"
+        kws = ['kw1', 'kw2']
+        self.db.create_note(note_name, note_text)
+        self.db.add_note_keywords(note_name, kws)
+        kw = self.db.get_note_keywords(note_name)
 
 if __name__ == '__main__':
     unittest.main()
